9b63ff276ef8f7e0faf966e07692f2d602ffcae9
Steve Smith <ssmith@chariotsolutions.com>
2017-07-13 09:17:08 -0400

    initial code

--- AppIconSwitch/ViewController.swift
+++ AppIconSwitch/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if UIApplication.shared.supportsAlternateIcons {
+            print("I can do it")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func baseballIcon(_ sender: Any) {
+        switchIcon(name: "baseball")
+    }
+
+    @IBAction func hockeyIcon(_ sender: Any) {
+        switchIcon(name: "hockey")
+    }
+
+
+    @IBAction func basketballIcon(_ sender: Any) {
+        switchIcon(name: "basketball")
+    }
+
+    @IBAction func resetIcon(_ sender: Any) {
+        switchIcon(name: nil)
+    }
+
+    private func switchIcon(name: String?) {
+        UIApplication.shared.setAlternateIconName(name) { (error: Error?) in
+            guard let error = error else {
+                print("success")
+                return
+            }
+            print("switching icon to: \(String(describing: name)) encountered and error: \(error)")
+        }
+    }
 
 }
 
